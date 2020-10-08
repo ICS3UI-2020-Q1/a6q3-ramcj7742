4decd85db33a82b4855bd9106472e03d06a1dcf5
ramcj7742 <ramcj7742@wrdsb.ca>
2020-10-08 20:58:41 +0000

    Finished largest number selection

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * selects largest number from array
+ * @author Jaden Ramcharan
  */
 public class Main {
 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //creates variable for largest number
+    int largeNum = 0;
+
+    //prompts user to enter integers
+    System.out.println("Please enter in 10 integers to put into the array");
+
+    //creates array
+    int array[] = new int[10];
+
+    //loop that puts numbers into array
+    for(int i = 0; i < array.length; i++){
+      array[i] = input.nextInt();
+      if(array[i] > largeNum){
+        largeNum = array[i];
+      }
+    }
+    //tells user the larges number
+    System.out.println("The largest number is " + largeNum);
     
   }
 }
